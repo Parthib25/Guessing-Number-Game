6c2f5c772ecb5e8cb3cb4017a12f1a69f5e744ef
Parthib Goswami <93084505+Parthib25@users.noreply.github.com>
2021-11-01 10:00:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+document.querySelector(".correctnumber");
+let x = 0,
+  y = 0,
+  z = 20;
+let highscore = 0;
+document.querySelector(".check-btn").addEventListener("click", function () {
+  x = Number(document.querySelector(".guess").value);
+  y = Math.floor(Math.random() * 21);
+
+  if (x === y) {
+    document.querySelector(".Number").textContent = String(y);
+    document.querySelector(".correctnumber").textContent =
+      "CORRECT NUMBER!💯💯💯";
+    document.querySelector("article").style.backgroundColor = "green";
+    if (z == 20) {
+      z = 20;
+    } else {
+      z++;
+    }
+    if (z <= 0) {
+      document.querySelector(".correctnumber").textContent = "You lost mate!😥";
+    }
+    document.querySelector(".sc").textContent = z;
+    if (z > highscore) {
+      highscore = z;
+      document.querySelector(".h").textContent = highscore;
+    }
+  }
+  if (x != y) {
+    document.querySelector(".Number").textContent = "?";
+    document.querySelector(".correctnumber").textContent =
+      "WRONG NUMBER MATE❌!";
+    document.querySelector("article").style.backgroundColor = "#af111c";
+    if (z == 20) {
+      z--;
+    } else {
+      z--;
+    }
+    if (z <= 0) {
+      document.querySelector(".correctnumber").textContent = "You lost mate!😥";
+    }
+    document.querySelector(".sc").textContent = z;
+    if (z > highscore) {
+      highscore = z;
+      document.querySelector(".h").textContent = highscore;
+    }
+  }
+  if (!x) {
+    document.querySelector(".Number").textContent = "❌";
+    document.querySelector(".correctnumber").textContent = "Error 👿";
+    document.querySelector("article").style.backgroundColor = "black";
+    z--;
+    document.querySelector(".sc").textContent = "Invalid number";
+    if (z <= 0) {
+      document.querySelector(".correctnumber").textContent = "You lost mate!😥";
+    }
+    document.querySelector(".sc").textContent = z;
+  }
+  if (x < 0) {
+    document.querySelector(".Number").textContent = "😤";
+    document.querySelector(".sc").textContent = "First See the top right 🤦‍♂️";
+    document.querySelector("article").style.backgroundColor = "darkblue";
+    z--;
+    document.querySelector(".correctnumber").textContent = "Invalid number";
+    if (z <= 0) {
+      document.querySelector(".correctnumber").textContent = "You lost mate!😥";
+    }
+    document.querySelector(".sc").textContent = z;
+  }
+});
+document.querySelector(".again").addEventListener("click", function () {
+  document.querySelector("article").style.backgroundColor = "black";
+  document.querySelector(".Number").textContent = "?";
+  document.querySelector(".correctnumber").textContent = "Start Guessing....";
+  document.querySelector(".sc").textContent = "0";
+  z = 20;
+});
+document.querySelector(".Number").textContent = "?";
